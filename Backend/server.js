7b49dc4a7b98d5e1ca97aff1614924ff9964ae0b
Sindbad <38933770+sanjkn@users.noreply.github.com>
2020-05-01 06:37:03 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,7 @@ require('dotenv').config();
 // bring routes
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
+
 const tagRoutes = require('./routes/tag');
 const formRoutes = require('./routes/form');
 
@@ -35,8 +34,7 @@ if (process.env.NODE_ENV === 'development') {
 // routes middleware
 app.use('/api', blogRoutes);
 app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
+
 app.use('/api', tagRoutes);
 app.use('/api', formRoutes);
 
